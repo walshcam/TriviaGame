d193cc387c1cc8571151eb2d827ef599434c9efa
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-02 21:40:08 -0400

    Created Outline

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+//Create objects - question, answer1, answer2, answer3, answer4
+
+//Create array of objects
+
+
+
+
+
+
+//Create Timer function for each question
+
+//Create Countdown function
+
+
+
+//Create function that will display question - Timer function
+
+//Create function for congratulation screen - 3 seconds
+
+//Create function for wrong answer screen - 3 seconds
+
+//Create function for ran out of time screen - 3 seconds
+
+//Create function that tracks correct / incorrect / timeout answers
+
+
+
+//*************ACTIONS ALGORITHM******************** */
+
+//Get Ready / Explain Rules Screen
+
+//Question Screen
+
+//Congratulations Screen
+
+//Right/Wrong Screen
+
+
+
+//*****************EXTRA STUFF*************************** 
+
+// ### Create a README.md
+
+// Add a `README.md` to your repository describing the project. Here are some resources for creating your `README.md`. Here are some resources to help you along the way:
+
+// * [About READMEs](https://help.github.com/articles/about-readmes/)
+
+// * [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
+
+// - - -
+
+// ### Add To Your Portfolio
+
+// After completing the homework please add the piece to your portfolio. Make sure to add a link to your updated portfolio in the comments section of your homework so the TAs can easily ensure you completed this step when they are grading the assignment. To receive an 'A' on any assignment, you must link to it from your portfolio.
+
+// - - -
+
+
